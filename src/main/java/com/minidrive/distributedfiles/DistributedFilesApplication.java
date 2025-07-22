a50120298cd9b21c7bf8edee5af5cf5ee4ed8a14
prodXCE <bhattacharjeeakash71@gmail.com>
2025-07-22 14:40:56 +0530

    Initial commit: Generate Spring Boot project structure

---
+++ src/main/java/com/minidrive/distributedfiles/DistributedFilesApplication.java
@@ -0,0 +1,13 @@
+package com.minidrive.distributedfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minidrive/distributedfiles/DistributedFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minidrive.distributedfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
